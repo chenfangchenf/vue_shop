e682a4531a85d2828eb5cfde42f11b201f33bdb2
e <1961630956@qq.com>
2021-11-30 20:41:49 +0800

    finish report

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,7 @@ import Params from '../components/goods/Params.vue'
 import GoodsList from '../components/goods/List.vue'
 import GoodsAdd from '../components/goods/AddGoods.vue'
 import Order from '../components/order/order.vue'
+import Reports from '../components/report/Reports.vue'
 Vue.use(VueRouter)
 
 const routes = [
@@ -32,8 +33,8 @@ const routes = [
       { path: '/params', component: Params },
       { path: '/goods', component: GoodsList },
       { path: '/goods/add', component: GoodsAdd },
-      { path: '/orders', component: Order }
-
+      { path: '/orders', component: Order },
+      { path: '/reports', component: Reports }
     ]
   }
 
